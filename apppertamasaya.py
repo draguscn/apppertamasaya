f47ecdc909c20c678fa0aa7d4387bd4253e43348
draguscn <draguscn@gmail.com>
2025-05-11 08:22:07 +0700

    Versi awal aplikasi hello streamlit apppertamasaya.py

---
+++ apppertamasaya.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Halo Dunia Web Saya!")
+st.write("Ini adalah halaman web pertama saya yang dibuat dengan Python dan Streamlit.")
+st.write("Hore, berhasil! 🎉")
+
+st.balloons()
